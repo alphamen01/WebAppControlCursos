0c7b29b51b10fdb64c9c75866dc13855115130f2
alphamen01 <lesg.2018@hotmail.com>
2023-07-15 21:47:21 -0500

    Creacion de proyecto, creacion de pagina razor Cursos y Contactar, creacion del modelo curso

---
+++ Models/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAppControlCursos.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string Uri { get; set; }
+    }
+}
---
+++ Pages/Contactar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppControlCursos.Pages
+{
+    public class ContactarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
