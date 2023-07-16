0b7780819063c3a76196dfa75060acde1256f780
alphamen01 <lesg.2018@hotmail.com>
2023-07-15 22:32:37 -0500

    Creacion de Interface ICoursesProvider y clase FakeCoursesProvider

---
+++ Interfaces/ICoursesProvider.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebAppControlCursos.Models;
+
+namespace WebAppControlCursos.Interfaces
+{
+    public interface ICoursesProvider
+    {
+        Task<ICollection<Course>> GetAllAsync();
+        Task<Course> GetAsync(int id);
+        
+    }
+}
---
+++ Providers/FakeCoursesProvider.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebAppControlCursos.Interfaces;
+using WebAppControlCursos.Models;
+
+namespace WebAppControlCursos.Providers
+{
+    public class FakeCoursesProvider : ICoursesProvider
+    {
+        private readonly List<Course> repo = new List<Course>();
+        public FakeCoursesProvider()
+        {
+            repo.Add(new Course()
+            {
+                Id = 1,
+                Name = "Sistemas Operativos",
+                Author= "Luis Sanchez",
+                Description= "Tecnologia",
+                Uri = "https://github.com/alphamen01"
+            });
+
+            repo.Add(new Course()
+            {
+                Id = 2,
+                Name = "Java",
+                Author = "Luis Geronimo",
+                Description = "Programacion",
+                Uri = "https://github.com/alphamen01"
+            });
+
+            repo.Add(new Course()
+            {
+                Id = 3,
+                Name = "C#",
+                Author = "Luis Tejada",
+                Description = "Programacion",
+                Uri = "https://github.com/alphamen01"
+            });
+
+        }
+
+        public Task<ICollection<Course>> GetAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Course> GetAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
