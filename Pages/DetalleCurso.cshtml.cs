d61ddb0ab98f2ea6f30fe449a94b80cc66af20ad
alphamen01 <lesg.2018@hotmail.com>
2023-07-16 19:28:12 -0500

    implementacion de Tag Helpers para navegar entre paginas

--- Pages/DetalleCurso.cshtml.cs
+++ Pages/DetalleCurso.cshtml.cs
@@ -1,15 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+using WebAppControlCursos.Interfaces;
 using WebAppControlCursos.Models;
 
 namespace WebAppControlCursos.Pages
 {
     public class DetalleCursoModel : PageModel
     {
+        private readonly ICoursesProvider coursesProvider;
+
         public Course Course { get; set; }
-        public void OnGet()
+
+        public DetalleCursoModel(ICoursesProvider coursesProvider)
         {
-            Course = new Course();
+            this.coursesProvider = coursesProvider;
+        }
+        public async Task<ActionResult> OnGet(int id)
+        {
+            var course = await coursesProvider.GetAsync(id);
+            if (course != null)
+            {
+                Course = course;
+                return Page();
+            }
+
+            return RedirectToPage("Cursos");
         }
     }
 }
